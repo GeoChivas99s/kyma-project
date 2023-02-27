c0ddcd1f30368355d1a7f88b5b0fb6ee1f5139b9
Geovane Chivas <geovane.chivaluca@drcash.com.br>
2023-02-27 12:38:42 +0100

    feat: add icons into tabs

--- src/pages/routes/main.tsx
+++ src/pages/routes/main.tsx
@@ -4,19 +4,31 @@ import SignUp from "../SignUp/SignUp";
 import { Teste } from "../teste/teste";
 import { Teste1 } from "../teste1/teste";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 export function Main() {
   return (
     <Tab.Navigator
-    screenOptions={ ({route})=>({
-      tabBarIcon: ({ focused, color, size }) => {
-        let iconName;
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName = "";
 
-      }
-      
-    })}
+          if (route.name === "Home") {
+            iconName = focused
+              ? "ios-information-circle"
+              : "ios-information-circle-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "ios-list" : "ios-list-outline";
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
     >
       <Tab.Screen name="Teste" component={Teste} />
       <Tab.Screen name="Teste1" component={Teste1} />
